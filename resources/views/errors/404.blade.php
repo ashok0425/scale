d348af7f2f46cb31123d5c03e959749a1d2fe314
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-06-23 09:51:07 +0545

    404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 | ScaleDux</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" crossorigin href="{{asset('frontend/assets/main.css')}}">
+
+    <style>
+
+.page_404{ padding:40px 0; background:#fff; font-family: 'Arvo', serif;
+}
+
+.page_404  img{ width:100%;}
+
+.four_zero_four_bg{
+
+ background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+    height: 400px;
+    background-position: center;
+ }
+
+
+ .four_zero_four_bg h1{
+ font-size:80px;
+ }
+
+  .four_zero_four_bg h3{
+			 font-size:80px;
+			 }
+
+			 .link_404{
+	color: #fff!important;
+    padding: 10px 20px;
+    /* background: #39ac31; */
+    text-decoration: none;
+    margin: 20px 0;
+    display: inline-block;}
+	.contant_box_404{ margin-top:-50px;}
+    </style>
+
+</head>
+<body>
+    <section class="page_404 text-center mx-auto">
+	<div class="container">
+		<div class="row">
+		<div class="col-md-10 mx-auto text-center ">
+		<div class="col-md-10 mx-auto  text-center">
+		<div class="four_zero_four_bg">
+			<h1 class="text-center ">404</h1>
+
+
+		</div>
+
+		<div class="contant_box_404">
+		<h3 class="h2">
+		Look like you're lost
+		</h3>
+
+		<p>the page you are looking for not avaible!</p>
+
+		<a href="/" class="link_404 btn-primary hover:shadow-brand-purple/60 group hover:shadow-lg">Go to Home</a>
+	</div>
+		</div>
+		</div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
