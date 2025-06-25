567efb8e3dd8d56cfbc340d24ceec6850b53e02f
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-06-25 16:57:52 +0545

    dynamic here link

--- resources/views/frontend/inc/total_value.blade.php
+++ resources/views/frontend/inc/total_value.blade.php
@@ -4,7 +4,7 @@
                   <div class="flex items-center justify-between gap-4 max-sm:flex-col">
                     <h3 class="font-inter text-lg leading-7 font-bold">Total Values:</h3>
                     <div class="flex items-center gap-2">
-                      <span class="text-body-2 text-sm line-through">₹3,07,000/-</span>
+                      <span class="text-body-2 text-sm line-through">₹799/-</span>
                       <h3 class="dh-3 font-stretch-condensed">₹399</h3>
                     </div>
                   </div>
@@ -12,7 +12,16 @@
                     Join the exclusive network of forward-thinking professionals positioning
                     themselves at the forefront of the new service economy. These privileges
                     disappear when we launch – lock them in now or miss them forever. You can
-                    download the value documentation <a href="#" class="underline text-purple">here</a>
+                    download the value documentation
+                    @if (request()->path()=='freelancer')
+                         <a href="{{url('/freelancers-agencies-benefit')}}" class="underline text-purple">here</a>
+                    @endif
+                    @if (request()->path()=='founders')
+                         <a href="{{url('/founders-benefit')}}" class="underline text-purple">here</a>
+                    @endif
+                    @if (request()->path()=='investors')
+                         <a href="{{url('/enabler-investor-mentor-benefits')}}" class="underline text-purple">here</a>
+                    @endif
                   </p>
                   <div class="mt-3 flex items-center justify-between gap-4">
                     <div class="gradient-border-animated rounded-lg px-6 py-1">
